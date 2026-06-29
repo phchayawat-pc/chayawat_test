9c2eb1011c284605c06ba12f2fd066d98ebb0828
chayawat.p <Chayawat.Phengsuk@truecorp.co.th>
2026-06-29 12:12:29 +0700

    test

--- src/main/java/th/co/chayawat/commonapi/cms/jpa/entity/Users.java
+++ src/main/java/th/co/chayawat/commonapi/cms/jpa/entity/Users.java
@@ -3,7 +3,6 @@ package th.co.chayawat.commonapi.cms.jpa.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
--- src/main/java/th/co/chayawat/commonapi/service/UserService.java
+++ src/main/java/th/co/chayawat/commonapi/service/UserService.java
@@ -2,17 +2,7 @@ package th.co.chayawat.commonapi.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import th.co.chayawat.commonapi.cms.jpa.entity.Users;
-import th.co.chayawat.commonapi.cms.jpa.repository.UserRepository;
-import th.co.chayawat.commonapi.dto.UsersDto;
-import th.co.chayawat.commonapi.model.UsersRsp;
-
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserService {
